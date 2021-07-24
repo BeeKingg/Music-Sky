b46a3a44c37f87ccb83d1227bebe62bced36ff2d
BeeKingg <87557652+BeeKingg@users.noreply.github.com>
2021-07-25 06:45:14 +0700

    Update private.py

--- MusicKen/modules/private.py
+++ MusicKen/modules/private.py
@@ -6,11 +6,24 @@ from MusicKen.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROU
 logging.basicConfig(level=logging.INFO)
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
-def _start(client, message):
-    client.send_message(message.chat.id,
-        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
-        parse_mode="markdown",
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_sticker("CAACAgUAAxkBAAFF-KFg-jaEvlhu_kNknYQjxsuyDvp--AACjAMAAtpWSVeocCICILIfRSAE")
+    await message.reply_text(
+        f"""👋🏻 Hallo, Nama saya [{PROJECT_NAME}](https://telegra.ph/file/ed136c19e7f6afddb4912.jpg)
+Dikekolah oleh @{OWNER}
+┈───────────────────┈
+☑️ Saya memiliki banyak fitur untuk anda yang suka lagu
+🔘 Memutar lagu di group 
+🔘 Mendownload lagu
+🔘 Mencari link youtube
+🔘 Mencari lirik lagu
+┈───────────────────┈
+☑️ Klik tombol bantuan untuk informasi lebih lanjut
         reply_markup=InlineKeyboardMarkup(
             [
                 [
