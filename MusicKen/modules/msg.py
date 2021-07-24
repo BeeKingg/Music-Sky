58bc94245ea4765fdce1254c508eb2d208bf9360
BeeKingg <87557652+BeeKingg@users.noreply.github.com>
2021-07-25 02:36:49 +0700

    Update msg.py

--- MusicKen/modules/msg.py
+++ MusicKen/modules/msg.py
@@ -4,7 +4,7 @@ class Messages():
       HELP_MSG = [
         ".",
 f"""
-**👋🏻 Hai Selamat Datang Kembali Di [{PROJECT_NAME}](https://telegra.ph/file/ed136c19e7f6afddb4912.jpg)
+**👋🏻 Hai Selamat Datang Kembali Di [{PROJECT_NAME}](https://telegra.ph/file/2edffd441802313ea985f.jpg)
 
 ⚪️ {PROJECT_NAME} Dapat Memutar Musik Di Obrolan Suara Grup Anda Serta Obrolan Suara Saluran
 
